22bbb9bf02e20d097879138ba2c340b980fcb9c6
Manannikov-Nikita <55488385+Manannikov-Nikita@users.noreply.github.com>
2020-02-19 21:44:25 +0300

    New Vk Bot
    Добавил меню боту настроил его нормальную работу

--- Vk_Bot/main.py
+++ Vk_Bot/main.py
@@ -1,43 +1,50 @@
 import vk_api
+import random
+from datetime import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 vk_session = vk_api.VkApi(token='12153e7f74d0c0257192e7f5b525f894cfd507168976a15b0d3490235ff76bd85b2a6fa05b42f9cd6737b')
 
 longPoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+
+def create_keyboard(response):
+    if response == 'начать':
+        keyboard = VkKeyboard(one_time=False)
+
+        keyboard.add_button('Отправить статью', color=VkKeyboardColor.DEFAULT)
+        keyboard.add_line()
+        keyboard.add_button('Список команд', color=VkKeyboardColor.DEFAULT)
+        keyboard.add_button('Инфо', color=VkKeyboardColor.DEFAULT)
+
+        keyboard = keyboard.get_keyboard()
+        return keyboard
+
+
 for event in longPoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        atchs = event.object['attachments']
-
-        if atchs:
-            for atch in atchs:
-                if atch['type'] == 'dod':
-                    document = atch['doc']
-
-
-
-
-# for event in longpoll.listen():
-#     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-#         if event.text == 'Соси хуй' or event.text == 'Пидр':
-#             if event.from_user:
-#                 vk.messages.send(
-#                     user_id=event.user_id,
-#                     random_id=random.randint(1, 280000),
-#                     message='Сам соси хуй'
-#                 )
-#
-#             elif event.from_chat:
-#                 vk.messages.send(
-#                     chat_id=event.chat_id,
-#                     random_id=random.randint(1, 280000),
-#                     message='Сам пидр'
-#                 )
-#
-#         if event.attachments == "dock.docx" and event.from_user:
-#             vk.messages.send(
-#                 user_id=event.user_id,
-#                 random_id=random.randint(1, 280000),
-#                 message='Сам соси хуй'
-#             )
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        print('Message sent: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
+        response = event.text.lower()
+        keyboard = create_keyboard(response)
+        print(response)
+        if response == 'начать':
+            vk_session.method(
+                'messages.send',
+                {
+                    'user_id': event.user_id,
+                    'message': 'Здравстуйте',
+                    'random_id': random.randint(1, 2800000),
+                    'keyboard': keyboard
+                }
+            )
+        else:
+            vk_session.method(
+                'messages.send',
+                {
+                    'user_id': event.user_id,
+                    'message': 'Я вас не понимаю',
+                    'random_id': random.randint(1, 2800000)
+                }
+            )
