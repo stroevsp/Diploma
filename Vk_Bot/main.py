cbb3d8a7677ae8ba8d55f542a71975b0e8a1048a
Manannikov-Nikita <55488385+Manannikov-Nikita@users.noreply.github.com>
2020-02-24 18:36:32 +0300

    Vk_bot update
    Добавлен функционал получения и скачивания файлов.

--- Vk_Bot/main.py
+++ Vk_Bot/main.py
@@ -1,5 +1,6 @@
 import vk_api
 import random
+import requests
 from datetime import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
@@ -14,7 +15,7 @@ def create_keyboard(response):
     if response == 'начать':
         keyboard = VkKeyboard(one_time=False)
 
-        keyboard.add_button('Отправить статью', color=VkKeyboardColor.DEFAULT)
+        keyboard.add_button('Отправить статью', color=VkKeyboardColor.POSITIVE)
         keyboard.add_line()
         keyboard.add_button('Список команд', color=VkKeyboardColor.DEFAULT)
         keyboard.add_button('Инфо', color=VkKeyboardColor.DEFAULT)
@@ -24,12 +25,32 @@ def create_keyboard(response):
 
 
 for event in longPoll.listen():
+
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         print('Message sent: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
         response = event.text.lower()
         keyboard = create_keyboard(response)
+        doc = event.attachments
+        print(doc)
         print(response)
-        if response == 'начать':
+
+        if doc != {}:
+            doc_info = vk_session.method(
+                'messages.getById',
+                {
+                    'message_ids': event.message_id,
+                }
+
+            )
+            url = doc_info['items'][0]['attachments'][0]['doc']['url']
+            download = requests.get(url)
+            try:
+                test = download.text.encode('latin1').decode('cp1251')
+                print(test)
+            except:
+                print('error')
+
+        elif response == 'начать':
             vk_session.method(
                 'messages.send',
                 {
@@ -39,6 +60,17 @@ for event in longPoll.listen():
                     'keyboard': keyboard
                 }
             )
+
+        elif response == 'отправить статью':
+            vk_session.method(
+                'messages.send',
+                {
+                    'user_id': event.user_id,
+                    'message': 'Скопируйте текст вашей статьи в этот диалог',
+                    'random_id': random.randint(1, 2800000),
+                }
+            )
+
         else:
             vk_session.method(
                 'messages.send',
